34121986ed4f66a415a77eb34ac36a0194e1e206
Morillo Boscan Santiago Javier <santiagomorillob@hotmail.com>
2025-09-20 23:55:19 -0400

    creando schema de user

---
+++ backend/src/schemas/user_schema.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, EmailStr, validator
+import re
+
+class UserBase(BaseModel):
+    name: str
+    age: int 
+    username:str
+    
+    
+class UserCreate(UserBase):
+    email: EmailStr
+    password: str
+    @validator('email')
+    def email_regex(cls, v):
+        if not re.match(r"^[\w\.-]+@[\w\.-]+\.\w+$", v):
+            raise ValueError('Email inválido')
+        return v
+
+    @validator('password')
+    def password_regex(cls, v):
+        if not re.match(r'^(?=.*[A-Z])(?=.*\d).{8,}$', v):
+            raise ValueError('La contraseña debe tener al menos 8 caracteres, una mayúscula y un número')
+        return v
+
+class UserRead(UserBase):
+    email: EmailStr
